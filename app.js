b863a9b32320e26111d970472ab3fd6f4ce2aa57
Daniel <danielsquinteros@gmail.com>
2021-11-04 20:55:10 -0300

    feat: the add folder structure, controllers, middlewares, helpers, models, routes

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+require('dotenv').config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(port, () => {
+  console.log(`Aplicación en el puerto http://localhost:${port}`);
 });
 
 module.exports = app;
